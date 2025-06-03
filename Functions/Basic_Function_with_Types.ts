cc12ace2aafc95512bc480d6c07f87715dcddd1e
Pop2323 <omar4500455@gmail.com>
2025-06-03 20:18:32 +0300

    Basic_Function_With_Type

---
+++ Functions/Basic_Function_with_Types.ts
@@ -0,0 +1,6 @@
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+add(5, 10); // returns 15
+// add(5, "10"); // Error: Argument of type 'string' is not assignable to parameter of type 'number'.
