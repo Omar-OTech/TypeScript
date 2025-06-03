294635f2312783a3fb66e3fe4ee376e80638d67c
Pop2323 <omar4500455@gmail.com>
2025-06-03 19:40:28 +0300

    TypeScript

---
+++ Basic Types.ts
@@ -0,0 +1,9 @@
+let id: number = 101;
+let username: string = "bop";
+let isActive: boolean = true;
+
+let user: { id: number; username: string; isActive: boolean } = {
+    id : 101,
+    username: "bop",
+    isActive: true
+}
