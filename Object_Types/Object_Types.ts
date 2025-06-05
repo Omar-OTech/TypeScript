48cfec0699a10bcc63a0443c635212968c962dc1
Pop2323 <omar4500455@gmail.com>
2025-06-05 21:19:19 +0300

    Object_types

---
+++ Object_Types/Object_Types.ts
@@ -0,0 +1,9 @@
+let userInfo: { id: number; name: string; isAdmin: boolean } = {
+  id: 1,
+  name: "Bop",
+  isAdmin: true
+};
+
+
+console.log(userInfo);
+// Output: { id: 1, name: 'Bop', isAdmin: true }
